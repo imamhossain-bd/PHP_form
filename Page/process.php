6ffb6bcc1e651bab52e27c3e1278dbd5a0943636
imamhossain-bd <imamhossain0098@gmail.com>
2024-11-23 11:34:27 +0600

    table

--- Page/process.php
+++ Page/process.php
@@ -1,89 +1,64 @@
 <?php
-// class Process {
-//     private $name;
-//     private $email;
-//     private $password;
+    class Process{
+        private $name;
+        private $email;
+        private $password;
 
-//     private static $file_path = "data.txt"; // Correct file extension
+        private static $file_path="data.txt";
 
-//     // Constructor to initialize properties
-//     function __construct($name, $email, $password) {
-//         $this->name = $name;
-//         $this->email = $email;
-//         $this->password = $password;
-//     }
+        function __construct($names, $emails, $passwords){
+            $this->name = $names;
+            $this->email = $emails;
+            $this->password = $passwords;
+        }
 
-//     // Convert object data to CSV format
-//     function csv() {
-//         return "$this->name,$this->email,$this->password" . PHP_EOL;
-//     }
+        public function propertyPass() {
+            return $this->name.",". $this->email.",". $this->password.PHP_EOL;
+        }
+        
 
-//     // Save data to the file
-//     function save() {
-//         // Append data to the file
-//         file_put_contents(self::$file_path, $this->csv(), FILE_APPEND);
-//     }
+        public function save(){
+            $processes= file(self::$file_path);
+            file_put_contents(self::$file_path, $this->propertyPass(), FILE_APPEND);
+        }
 
-//     // Display all saved data
-//     public static function display_process() {
-//         if (!file_exists(self::$file_path)) {
-//             echo "No data available.";
-//             return;
-//         }
 
-//         $processes = file(self::$file_path, FILE_IGNORE_NEW_LINES);
-//         echo "<b>Name | Email</b><br/>";
-//         foreach ($processes as $process) {
-//             list($name, $email) = explode(",", $process);
-//             echo "$name | $email<br/>";
-//         }
-//     }
-// }
+        public static function display_process() {
+            $processes = file(self::$file_path);
+        
+            // Start the table
+            echo "
+            <table style='margin-left: 38%; padding-bottom: 10; font-size: 1.25rem; border-collapse: collapse;'>
+                <thead>
+                    <tr>
+                        <th style='border: 1px solid black; padding: 8px;'>Name</th>
+                        <th style='border: 1px solid black; padding: 8px;'>Email</th>
+                        <th style='border: 1px solid black; padding: 8px;'>Password</th>
+                    </tr>
+                </thead>
+                <tbody>";
+        
+            // Iterate over each process
+            foreach ($processes as $process) {
+                list($name, $email, $password) = explode(",", trim($process));
+                echo "
+                <tr>
+                    <td style='border: 1px solid black; padding: 8px;'>$name</td>
+                    <td style='border: 1px solid black; padding: 8px;'>$email</td>
+                    <td style='border: 1px solid black; padding: 8px;'>$password</td>
+                </tr>";
+            }
+        
+            // Close the table
+            echo "
+                </tbody>
+            </table>";
+        }
+        
+    }
+
 ?>
 
 
-<?php
-class Process {
-    private $name;
-    private $email;
-    private $password;
 
-    private static $file_path = "data.txt"; // File to store data
-
-    // Constructor to initialize properties
-    function __construct($name, $email, $password) {
-        $this->name = $name;
-        $this->email = $email;
-        $this->password = $password;
-    }
-
-    // Convert object data to CSV format
-    function csv() {
-        return "$this->name,$this->email,$this->password" . PHP_EOL;
-    }
-
-    // Save data to the file
-    function save() {
-        file_put_contents(self::$file_path, $this->csv(), FILE_APPEND);
-    }
-
-    // Display all saved data
-    public static function display_process() {
-        if (!file_exists(self::$file_path)) {
-            echo "<p class='text-center'>No data available.</p>";
-            return;
-        }
-
-        $processes = file(self::$file_path, FILE_IGNORE_NEW_LINES);
-        echo "<table class='table-auto border-collapse border border-gray-300 mx-auto'>";
-        echo "<thead><tr><th class='border border-gray-300 px-4 py-2'>Name</th><th class='border border-gray-300 px-4 py-2'>Email</th></tr></thead>";
-        echo "<tbody>";
-        foreach ($processes as $process) {
-            list($name, $email) = explode(",", $process);
-            echo "<tr><td class='border border-gray-300 px-4 py-2'>$name</td><td class='border border-gray-300 px-4 py-2'>$email</td></tr>";
-        }
-        echo "</tbody></table>";
-    }
-}
-?>
 
